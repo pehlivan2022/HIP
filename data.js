d1b504391ef8cb4ce73280bb2d93eca43d40ccbb
pehlivan2022 <99028696+pehlivan2022@users.noreply.github.com>
2024-04-15 02:15:45 +0200

    ristorante

--- data.js
+++ data.js
@@ -45,14 +45,20 @@ var APP_DATA = {
           "yaw": -2.8523484414627696,
           "pitch": 0.055395406128083025,
           "rotation": 0,
-          "target": "3-dji_0036"
+          "target": "3-dependance"
+        },
+        {
+          "yaw": -1.8716859060581328,
+          "pitch": 0.02374647874104774,
+          "rotation": 0.7853981633974483,
+          "target": "6-goletta-ricevimento"
         }
       ],
       "infoHotspots": [
         {
           "yaw": 1.1449952201008635,
           "pitch": 0,
-          "title": "Title",
+          "title": "uscita 1<div><br></div>",
           "text": "Text"
         }
       ]
@@ -120,8 +126,101 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "3-dji_0036",
-      "name": "DJI_0036",
+      "id": "3-dependance",
+      "name": "dependance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dji-sala-colazioni-4-__ritoccata",
+      "name": "DJI sala colazioni (4) __ritoccata",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-goletta-esterno_1",
+      "name": "goletta esterno_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-goletta-ricevimento",
+      "name": "GOLETTA RICEVIMENTO",
       "levels": [
         {
           "tileSize": 256,
